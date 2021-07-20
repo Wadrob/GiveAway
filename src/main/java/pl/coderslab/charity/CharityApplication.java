76198c244a07c6d7b74531842df79f0b77e920f6
Wadrob <dawidborda@gmail.com>
2021-07-20 12:45:36 +0200

    Added header and footer

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CharityApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CharityApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(CharityApplication.class, args);
     }
 
 }
