14f8201f8e6e548354665e548eade846a4f17ed8
Wadrob <dawidborda@gmail.com>
2021-07-22 14:09:10 +0200

    - Validation for Donation

--- src/main/java/pl/coderslab/charity/domain/model/Donation.java
+++ src/main/java/pl/coderslab/charity/domain/model/Donation.java
@@ -1,6 +1,7 @@
 package pl.coderslab.charity.domain.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +13,26 @@ public class Donation {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+    @Min(1) @Max(100)
     private Integer quantity;
     @OneToMany
+    @NotEmpty
     private List <Category> categories = new ArrayList<>();
     @ManyToOne
+    @NotEmpty
     private Institution institution;
+    @NotEmpty @NotBlank
+    @Size(max = 100, message = "Maksymalnie 100 znaków")
     private String street;
+    @NotEmpty @NotBlank
+    @Size(max = 30)
     private String city;
+    @NotBlank @NotEmpty
+    @Size(max = 6)
     private String zipCode;
+    @NotNull
     private LocalDateTime pickUpDateTime;
+    @Size (max = 300)
     private String pickUpComment;
 
-    //walidacja pól ( długości pól )
 }
