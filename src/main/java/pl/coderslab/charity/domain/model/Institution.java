1032c8ad5b274d2da9e5e264cc80573b5b7ff92b
Wadrob <dawidborda@gmail.com>
2021-07-22 14:05:46 +0200

    - Validation for Institution

--- src/main/java/pl/coderslab/charity/domain/model/Institution.java
+++ src/main/java/pl/coderslab/charity/domain/model/Institution.java
@@ -1,6 +1,9 @@
 package pl.coderslab.charity.domain.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table (name = "institutions")
@@ -9,6 +12,9 @@ public class Institution {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty @NotBlank
+    @Size (max = 100)
     private String name;
+    @Size(max = 500, message = "Maksymalnie 500 znaków")
     private String description;
 }
